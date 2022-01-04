c4c765ee43b5bb3a89be6a49ce9dc4deff8656ac
zhengy <zhengy>
2022-01-04 08:58:05 +0800

    递归合并排序

---
+++ src/main/java/com/armin/struct/RecursionSort.java
@@ -0,0 +1,43 @@
+package com.armin.struct;
+
+public class RecursionSort {
+    // https://www.cs.usfca.edu/~galles/visualization/ComparisonSort.html
+    public static void main(String[] args) {
+        int[] arr = {5, 54, 12, 78, 45, 9, 82, 62, 55, 8, 0, 6, -8, 101};
+        RecursionSort p = new RecursionSort();
+        p.mergesort(arr, 0, arr.length - 1);
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        // -8 0 5 6 8 9 12 45 54 55 62 78 82 101
+    }
+
+    /*将数组递归分为多个子数组，在依次排序合并*/
+    public void mergesort(int[] arr, int left, int right) {
+        if (left < right) {
+            int center = (left + right) >> 1;
+            mergesort(arr, left, center);
+            mergesort(arr, center + 1, right);
+            merge(arr, left, center, right);
+        }
+    }
+
+    public static void merge(int[] A, int l, int c, int r) {
+        int[] help = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = c + 1;
+        while (p1 <= c && p2 <= r) {
+            help[i++] = A[p1] <= A[p2] ? A[p1++] : A[p2++];
+        }
+        while (p1 <= c) {
+            help[i++] = A[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = A[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            A[l+i] = help[i];
+        }
+    }
+}
--- src/main/java/com/armin/think/concurrency/BasicThreads.java
+++ src/main/java/com/armin/think/concurrency/BasicThreads.java
@@ -1,13 +1,27 @@
 package com.armin.think.concurrency;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author zy
  * @version 2021/11/30
  */
 public class BasicThreads {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Thread thread = new Thread(new LiftOff());
         thread.start();
         System.out.println("Waiting for LiftOff");
+
+        SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date parse = format1.parse("2021-12-31 23:59:59");
+        System.out.println("parse1 = " + parse);
+        SimpleDateFormat format2 = new SimpleDateFormat("YYYY-MM-dd hh:mm:ss");
+        Date parse2 = format2.parse("2021-12-31 23:59:59");
+        System.out.println("parse1 = " + parse2);
+        SimpleDateFormat format3 = new SimpleDateFormat("YYYY-MM-DD hh:mm:ss");
+        Date parse3 = format3.parse("2021-12-31 23:59:59");
+        System.out.println("parse1 = " + parse3);
     }
 }
