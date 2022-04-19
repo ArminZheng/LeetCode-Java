0791864010567e448be8f8fccfd9fd208ba616a3
armin <arminzheng@qq.com>
2022-04-20 07:49:20 +0800

    StringJoiner

---
+++ src/main/java/com/armin/think/generics/StringJoinerGenerator.java
@@ -0,0 +1,34 @@
+package com.armin.think.generics;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+/**
+ * StringJoinerGenerator
+ *
+ * @author zy
+ * @version 2022/4/20
+ */
+public class StringJoinerGenerator {
+
+    public static void main(String[] args) {
+        String[] names = {"A", "B", "C", "D"};
+        StringJoiner stringJoiner = new StringJoiner(", ", "「", "」");
+        for (String name : names) {
+            stringJoiner.add(name);
+        }
+        System.out.println("stringJoiner = " + stringJoiner);
+
+        StringJoiner sj = new StringJoiner(":", "[", "]");
+        sj.add("George").add("Sally").add("Fred");
+        String desiredString = sj.toString();
+        System.out.println("desiredString = " + desiredString);
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+        String commaSeparatedNumbers =
+                numbers.stream().map(Object::toString).collect(Collectors.joining(", ", "[", "]"));
+        System.out.println("commaSeparatedNumbers = " + commaSeparatedNumbers);
+    }
+}
