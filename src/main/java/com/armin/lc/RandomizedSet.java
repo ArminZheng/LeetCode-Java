44cbb28330d5fa8ea807019cc4739299c1142ee3
armin <arminzheng@qq.com>
2022-05-29 04:34:17 +0800

    388 O(1) 时间插入、删除和获取随机元素

---
+++ src/main/java/com/armin/lc/RandomizedSet.java
@@ -0,0 +1,52 @@
+package com.armin.lc;
+
+/**
+ * 380. O(1) 时间插入、删除和获取随机元素
+ *
+ * @author zy
+ * @version 2022/4/21
+ */
+public class RandomizedSet {
+    /**
+     * RandomizedSet() 初始化 RandomizedSet 对象
+     */
+    public RandomizedSet() {
+
+    }
+
+    /**
+     * bool insert(int val) 当元素 val 不存在时，向集合中插入该项，并返回 true ；否则，返回 false 。
+     *
+     * @param val 元素
+     * @return 存在否
+     */
+    public boolean insert(int val) {
+        return false;
+    }
+
+    /**
+     * bool remove(int val) 当元素 val 存在时，从集合中移除该项，并返回 true ；否则，返回 false 。
+     *
+     * @param val 元素
+     * @return 存在否
+     */
+    public boolean remove(int val) {
+        return false;
+    }
+
+    /**
+     * int getRandom() 随机返回现有集合中的一项（测试用例保证调用此方法时集合中至少存在一个元素）。每个元素应该有 相同的概率 被返回。
+     *
+     * @return 随机返回现有集合中的一项
+     */
+    public int getRandom() {
+        return 0;
+    }
+}
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
--- src/main/java/com/armin/sword/CQueue.java
+++ src/main/java/com/armin/sword/CQueue.java
@@ -1,7 +1,7 @@
 package com.armin.sword;
 
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 用两个栈实现一个队列
@@ -33,5 +33,27 @@ class CQueue {
             out.push(in.pop());
         }
         return out.pop();
+    }
+
+    public static void main(String[] args) {
+        // only Integer boxed type
+        Integer[] num = new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        List<Integer> list = new ArrayList<>();
+        Collections.addAll(list, num);
+
+        int[] ints = new int[]{1,2,3,4,5,6};
+        // Cannot resolve method 'addAll(java.util.ArrayList<java.lang.Integer>, int[])'
+        // ArrayList<Integer> integers = new ArrayList<>();
+        // Collections.addAll(integers, ints);
+
+        // use Stream
+        List<Integer> collect = Arrays.stream(ints).boxed().collect(Collectors.toList());
+
+        // run slower
+        int[] res = collect.stream().mapToInt(Integer::intValue).toArray();
+        // faster little
+        // int[] result = new int[list.size()];
+        // AtomicInteger i = new AtomicInteger();
+        // list.forEach(e -> result[i.getAndIncrement()] = e);
     }
 }
